acc1fb75e97342367a765394606238048e07d18f
estudiante <estudiante@GGIECTEMWRK219.espol.edu.ec>
2023-05-25 16:50:48 -0500

    commit inicial

---
+++ app/src/main/java/com/example/appamst6/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.appamst6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edtUsuario, edtClave;
+    private Button btnLogin, btnRegistrarse;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtUsuario = (EditText) findViewById(R.id.edtUsuario);
+        edtClave = (EditText) findViewById(R.id.edtClave);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnRegistrarse = (Button) findViewById(R.id.btnRegistrarse);
+    }
+
+    private void registrarse(View view) {
+        Intent intent = new Intent(this, formulario_registro.class);
+        btnRegistrarse.setClickable(false);
+        startActivity(intent);
+        btnRegistrarse.setClickable(true);
+    }
+
+    private void login(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(),"Usted no cuenta con un usuario",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void onClick(View v) {
+        if(v.getId() == R.id.btnLogin){
+            Log.d("mensaje","ïngreso");
+            login(v);
+        } else if(v.getId() == R.id.btnRegistrarse) {
+            registrarse(v);
+        }
+    }
+}
